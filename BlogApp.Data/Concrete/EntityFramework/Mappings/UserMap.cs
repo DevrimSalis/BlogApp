0770a9580125a728f6dea8580bfa19c40bb2cca4
Devrim SALIŞ <derimsalis06@gmail.com>
2021-12-26 12:07:19 +0300

    Create first migration, adding unitofwork and result structure

--- BlogApp.Data/Concrete/EntityFramework/Mappings/UserMap.cs
+++ BlogApp.Data/Concrete/EntityFramework/Mappings/UserMap.cs
@@ -9,11 +9,11 @@ using Microsoft.EntityFrameworkCore.Metadata.Builders;
 
 namespace BlogApp.Data.Concrete.EntityFramework.Mappings
 {
-    public class UserMap:IEntityTypeConfiguration<User>
+    public class UserMap : IEntityTypeConfiguration<User>
     {
         public void Configure(EntityTypeBuilder<User> builder)
         {
-            builder.HasKey(u=>u.Id);
+            builder.HasKey(u => u.Id);
             builder.Property(u => u.Id).ValueGeneratedOnAdd();
             builder.Property(u => u.Email).IsRequired();
             builder.Property(u => u.Email).HasMaxLength(50);
@@ -41,6 +41,26 @@ namespace BlogApp.Data.Concrete.EntityFramework.Mappings
             builder.Property(u => u.Note).HasMaxLength(500);
             builder.HasOne<Role>(u => u.Role).WithMany(r => r.Users).HasForeignKey(u => u.RoleId);
             builder.ToTable("Users");
+            builder.HasData(new User
+            {
+                Id = 1,
+                RoleId = 1,
+                FirstName = "Devrim",
+                LastName = "SALIŞ",
+                UserName = "devrimsalis",
+                Email = "test@testmail.com",
+                IsActive = true,
+                IsDeleted = false,
+                CreatedByName = "InitialCreate",
+                CreatedDate = DateTime.Now,
+                ModifiedByName = "InitialCreate",
+                ModifiedDate = DateTime.Now,
+                Description = "İlk Admin Kullanıcı",
+                Note = "Admin Kullanıcısı",
+                PasswordHash = Encoding.ASCII.GetBytes("0192023a7bbd73250516f069df18b500"),
+                Picture = "https://encrypted-tbn0.gstatic.com/images?q=tbn%3AANd9GcSX4wVGjMQ37PaO4PdUVEAliSLi8-c2gJ1zvQ&usqp=CAU"
+            });
+
         }
     }
 }
---
+++ BlogApp.Shared/Utilities/Results/Concrete/DataResult.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BlogApp.Shared.Utilities.Results.Abstract;
+using BlogApp.Shared.Utilities.Results.ComplexTypes;
+
+namespace BlogApp.Shared.Utilities.Results.Concrete
+{
+    public class DataResult<T> : IDataResult<T>
+    {
+        public DataResult(ResultStatus resultStatus, T data)
+        {
+            ResultStatus = resultStatus;
+            Data = data;
+        }
+
+        public DataResult(ResultStatus resultStatus, string message, T data)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+        }
+
+        public DataResult(ResultStatus resultStatus, string message, T data, Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Data = data;
+            Exception = exception;
+        }
+
+        public ResultStatus ResultStatus { get; }
+        public string Message { get; }
+        public Exception Exception { get; }
+        public T Data { get; }
+    }
+}
